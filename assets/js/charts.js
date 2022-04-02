357d8d73a4b6002372192932d703a4aa03658163
alilynnh <90108950+alilynnh@users.noreply.github.com>
2022-04-02 10:21:45 -0500

    model charts done

--- assets/js/charts.js
+++ assets/js/charts.js
@@ -25,9 +25,26 @@ var mean_layout = {
     xaxis: {tickangle: -45, automargin:true},
     yaxis: {automargin:true},
     barmode: 'group',
-    paper_gb_color: '#f1f1f1',
-    plot_gbcolor: '#f1f1f1'};
+    paper_bgcolor: "rgba(0,0,0,0)",
+    plot_bgcolor: "rgba(0,0,0,0)"};
   
 Plotly.newPlot('mean-chart', mean_data, mean_layout);
 
 
+var featuredata = [{
+      x: ['FGM-TeamB', 'STL-TeamA', 'TO-TeamB', 'TO-TeamA', 'OR-TeamA', 'STL-TeamB', 
+            'BLK-TeamA', 'FTA-TeamA', 'MeanRank-TeamA', 'PF-TeamB'],
+      y: [.040422, .040183, 0.039949, .038769, .038166, .037045, .035694, 
+            .035234, .034098, .033546],
+      type: 'bar',
+      marker: { color: '#8A9B68', opacity:0.75}
+    }];
+
+var featurelayout = {
+    title: 'Top 10 Feature Importances',
+    xaxis: {automargin:true, tickangle: -45},
+    yaxis: {automargin:true},
+    paper_bgcolor: "rgba(0,0,0,0)",
+    plot_bgcolor: "rgba(0,0,0,0)"};
+  
+Plotly.newPlot('feature-chart', featuredata, featurelayout);
