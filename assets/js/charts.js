8b1129c9ec60500eebff0f525b1b698ec5790765
alilynnh <90108950+alilynnh@users.noreply.github.com>
2022-04-01 22:19:30 -0500

    added interactive plotly mean chart to dashboard

---
+++ assets/js/charts.js
@@ -0,0 +1,31 @@
+var mean1 = {
+    x: ['Score', 'Field Goals Made', 'Field Goals Attempted', '3 Pointers Made', '3 Pointers Attempted', 'Free Throws Made',
+    'Free Throws Attempted', 'Offensive Rebounds', 'Defensive Rebounds', 'Assists', 'Turnovers Committed', 'Steals',
+    'Blocks', 'Personal Fouls Committed'],
+    y: [84, 31, 63, 10, 25, 12, 17, 11, 22, 17, 12, 9, 4, 5],
+    name: '2021 Champion',
+    type: 'bar'
+};
+  
+var mean2 = {
+    x: ['Score', 'Field Goals Made', 'Field Goals Attempted', '3 Pointers Made', '3 Pointers Attempted', 'Free Throws Made',
+    'Free Throws Attempted', 'Offensive Rebounds', 'Defensive Rebounds', 'Assists', 'Turnovers Committed', 'Steals',
+    'Blocks', 'Personal Fouls Committed'],
+    y: [71, 25, 58, 7, 22, 13, 18, 8, 24, 13, 13, 6, 3, 5],
+    name: '2021 Other Teams',
+    type: 'bar'
+};
+  
+var mean_data = [mean1, mean2];
+  
+var mean_layout = {
+    title: 'Average Statistics for 2021 Regular Season',
+    xaxis: {tickangle: -45, automargin:true},
+    yaxis: {automargin:true},
+    barmode: 'group',
+    paper_gb_color: '#f1f1f1',
+    plot_gbcolor: '#f1f1f1'};
+  
+Plotly.newPlot('mean-chart', mean_data, mean_layout);
+
+
